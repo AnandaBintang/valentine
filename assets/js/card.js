c62da15e788dd81e294f023549d3884b6ebd86ff
Ananda Bintang <anandabintang4@gmail.com>
2024-02-14 02:12:39 +0700

    hidden button fix on card

--- assets/js/card.js
+++ assets/js/card.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
   var envelope = $("#envelope");
   var btn_open = $("#open");
   var btn_reset = $("#reset");
+  var reset_element = $(".reset");
 
   envelope.click(function () {
     open();
@@ -15,9 +16,11 @@ $(document).ready(function () {
 
   function open() {
     envelope.addClass("open").removeClass("close");
+    reset_element.removeClass("hidden");
   }
 
   function close() {
     envelope.addClass("close").removeClass("open");
+    reset_element.addClass("hidden");
   }
 });
